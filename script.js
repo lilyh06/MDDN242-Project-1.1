b94af90779ab3fafcd65f861f3b5ea432dd89c6a
lilyh06 <hagenlily@myvuw.ac.nz>
2026-03-19 09:39:57 +1300

    19/03

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.getElementById('btn-one').addEventListener('click', function () {
-  window.location.href = "mywork.html";
+  const target = document.getElementById("work-section");
+
+  // Scroll to the vertical center of the background image
+  const targetPosition = target.offsetTop + target.offsetHeight / 2 - window.innerHeight / 2;
+
+  window.scrollTo({
+    top: targetPosition,
+    behavior: "smooth"
+  });
 });
+
 
